f4af16fbfe74385a5e5eceb2a00f6382e6d64c08
Fanny Van Rell <fannyvanrell@gmail.com>
2020-04-17 15:29:45 +0200

    index

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/pyramide'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/pyramide/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/pyramide/src/app/app.component.html'));
   });
 
 // Start the app by listening on the default Heroku port
