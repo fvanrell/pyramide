7c9aafd3d170c2b3ec8f5fca53caddf9f528ada1
Fanny Van Rell <fannyvanrell@gmail.com>
2020-04-17 19:01:15 +0200

    tamer

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-/* eslint-disable */
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-var port = process.env.PORT || 3000;
-var app = express();
+// Serve static files....
+app.use(express.static(__dirname + '/dist/pyramide'));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/pyramide/index.html'));
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`server started port: ${port}`);
-  }
-});
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
