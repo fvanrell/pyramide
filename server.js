488ed27e6a9843583920d4fb48dee086250a6775
Fanny Van Rell <fannyvanrell@gmail.com>
2020-04-17 20:03:38 +0200

    once again

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
+// server.js
 const express = require('express');
-const path = require('path');
 const app = express();
-
-// Serve static files....
-app.use(express.static(__dirname + '/dist/pyramide'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/pyramide/index.html'));
-});
-
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/pyramide/'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
